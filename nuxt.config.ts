5cf98f19747ba86dd58516a50bf9a7edf8c5c200
b.pieren <b.pieren@socialblue.com>
2025-08-27 14:27:45 +0200

    chore: disable Nuxt dev tools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@ import { defineNuxtConfig } from 'nuxt/config';
 
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
-    devtools: { enabled: true },
     modules: [
         [
             '@nuxtjs/eslint-module',
